c47aa1b8f7b25a517a7d8e1501eb56de53f367e4
azeezdot123 <headofstate123@gmail.com>
2021-08-17 20:13:17 +0100

    ok

--- resume/settings.py
+++ resume/settings.py
@@ -133,6 +133,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'devolamilekan@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-
+EMAIL_HOST_PASSWORD = 'bwmgckwpcqmqsvbg'
 django_heroku.settings(locals())
