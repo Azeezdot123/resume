8f1efde16b1cde8e748dc047de8a181f990789ef
azeezdot123 <headofstate123@gmail.com>
2021-08-17 20:06:01 +0100

    added heroku settings

--- resume/settings.py
+++ resume/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +133,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'devolamilekan@gmail.com'
-EMAIL_HOST_PASSWORD = 'bwmgckwpcqmqsvbg'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
+django_heroku.settings(locals())
